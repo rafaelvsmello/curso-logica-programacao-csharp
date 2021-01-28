041d76070c3ec99ee919e9d8c622ade615fba523
rafaelvsmello <rmellov@gmail.com>
2021-01-28 09:36:24 -0300

    Commit do exercício 1001

--- Exercicio_1001/Program.cs
+++ Exercicio_1001/Program.cs
@@ -6,7 +6,14 @@ namespace Exercicio_1001
     {
         static void Main(string[] args)
         {
-            
+            int a, b, x;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            x = a + b;
+
+            Console.WriteLine("X = {0}", x);
         }
     }
 }
