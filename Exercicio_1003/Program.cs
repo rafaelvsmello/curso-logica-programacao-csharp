b61d716fd878ba3183cba4e11897021856f1f5de
rafaelvsmello <rmellov@gmail.com>
2021-01-28 14:04:31 -0300

    Commit do exercício 1003

---
+++ Exercicio_1003/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio_1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, soma;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            soma = a + b;
+            Console.WriteLine("SOMA = {0}", soma);
+        }
+    }
+}
