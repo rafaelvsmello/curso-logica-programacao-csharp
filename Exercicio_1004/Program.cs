bb4127ef4ed5df5b607101454ee652ffb84f35b9
rafaelvsmello <rmellov@gmail.com>
2021-01-28 16:39:58 -0300

    Commit dos exercícios 1004 e 1005

---
+++ Exercicio_1004/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio_1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, prod;
+
+            valor1 = int.Parse(Console.ReadLine());
+            valor2 = int.Parse(Console.ReadLine());
+
+            prod = valor1 * valor2;
+
+            Console.WriteLine("PROD = {0}", prod);
+        }
+    }
+}
---
+++ Exercicio_1005/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, media;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (a * 3.5) + (b * 7.5);
+            media /= 11;
+            Console.WriteLine("MEDIA = {0}", media.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
