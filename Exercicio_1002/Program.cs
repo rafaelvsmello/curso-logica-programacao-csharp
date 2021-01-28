8f0069b26fa8ff25bda2352c70d3692f53a86840
rafaelvsmello <rmellov@gmail.com>
2021-01-28 11:44:02 -0300

    Commit do exercício 1002

---
+++ Exercicio_1002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area, n = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = n * Math.Pow(raio, 2.0);
+            Console.WriteLine("A={0}", area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
