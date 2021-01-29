0809903c14783d99872b92461e2d111c6036e254
rafaelvsmello <rmellov@gmail.com>
2021-01-29 16:15:45 -0300

    Commit do exercício 1006

---
+++ Exercicio_1006/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, media;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (a * 2) + (b * 3) + (c * 5);
+            media /= 10;
+            Console.WriteLine("MEDIA = {0}", media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
