7c890413ea6547364bd3240cad4aaf1a0197bf16
rafaelvsmello <rmellov@gmail.com>
2021-01-29 21:26:31 -0300

    Commit dos exercícios 1016 e 1017

---
+++ Exercicio_1016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio_1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int km, minutos;
+
+            km = int.Parse(Console.ReadLine());
+            minutos = km * 2;
+            Console.WriteLine("{0} minutos", minutos);
+        }
+    }
+}
---
+++ Exercicio_1017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoViagem, velocidadeMedia;
+            double litrosNecessarios;
+
+            tempoViagem = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            litrosNecessarios = tempoViagem * velocidadeMedia / (double)12;
+            Console.WriteLine(litrosNecessarios.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
