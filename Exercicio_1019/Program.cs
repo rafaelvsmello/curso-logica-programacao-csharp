ea919764d47dfeb90f62fd01f4ba3b3725324de3
rafaelvsmello <rmellov@gmail.com>
2021-01-31 19:40:56 -0300

    Commit do exercício 1019

---
+++ Exercicio_1019/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, horas, minutos, segundos, resto;
+
+            n = int.Parse(Console.ReadLine());
+
+            horas = n / 3600;
+            resto = n % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine("{0}:{1}:{2}", horas, minutos, segundos);
+        }
+    }
+}
