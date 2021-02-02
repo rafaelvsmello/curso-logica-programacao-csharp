38771788da898800b3e7683f5f32a5d996317280
rafaelvsmello <rmellov@gmail.com>
2021-02-02 20:40:57 -0300

    Commit do exercício 1020

---
+++ Exercicio_1020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, anos, meses, resto;
+
+            n = int.Parse(Console.ReadLine());
+
+            anos = n / 365;
+            resto = n % 365;
+
+            meses = resto / 30;
+            resto = resto % 30;
+
+            Console.WriteLine("{0} ano(s)", anos);
+            Console.WriteLine("{0} mes(es)", meses);
+            Console.WriteLine("{0} dia(s)", resto);
+        }
+    }
+}
