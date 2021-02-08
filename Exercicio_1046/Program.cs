50d7571a57a6ba5580fe3dd745cd1678c5eeb7f3
rafaelvsmello <rmellov@gmail.com>
2021-02-08 17:08:59 -0300

    Commit dos exercícios 1046 e 1048

---
+++ Exercicio_1046/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio, fim, duracao;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            inicio = int.Parse(vet[0]);
+            fim = int.Parse(vet[1]);
+
+            if (inicio < fim)
+            {
+                duracao = fim - inicio;
+            }
+            else if (inicio > fim)
+            {
+                duracao = Math.Abs((1440 - ((inicio - fim) * 60)) / 60);                
+            }
+            else
+            {
+                duracao = 24;
+            }
+
+            Console.WriteLine("O JOGO DUROU {0} HORA(S)", duracao);
+        }
+
+    }
+}
---
+++ Exercicio_1048/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, reajuste, novoSalario;
+            int percentual;
+
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario >= 0 && salario <= 400.00)
+            {
+                percentual = 15;
+                reajuste = salario * percentual / 100;
+                novoSalario = salario + reajuste;
+            }
+            else if (salario >= 400.01 && salario <= 800.00)
+            {
+                percentual = 12;
+                reajuste = salario * percentual / 100;
+                novoSalario = salario + reajuste;
+            }
+            else if (salario >= 800.01 && salario <= 1200.00)
+            {
+                percentual = 10;
+                reajuste = salario * percentual / 100;
+                novoSalario = salario + reajuste;
+            }
+            else if (salario >= 1200.01 && salario <= 2000.00)
+            {
+                percentual = 7;
+                reajuste = salario * percentual / 100;
+                novoSalario = salario + reajuste;
+            }
+            else
+            {
+                percentual = 4;
+                reajuste = salario * percentual / 100;
+                novoSalario = salario + reajuste;
+            }
+
+            Console.WriteLine("Novo salario: {0}", novoSalario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: {0}", reajuste.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: {0} %", percentual);
+        }
+    }
+}
