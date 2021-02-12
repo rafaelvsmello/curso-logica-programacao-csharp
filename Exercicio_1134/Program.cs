ba345e8b3b9168d748cf4eea9b927d21c628edbc
rafaelvsmello <rmellov@gmail.com>
2021-02-12 19:48:47 -0300

    Commit do exercício 1134

---
+++ Exercicio_1134/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0, gasolina = 0, diesel = 0;
+            int combustivel = int.Parse(Console.ReadLine());
+
+            while (combustivel != 4)
+            {
+                switch (combustivel)
+                {
+                    case 1:
+                        alcool++;
+                        break;
+                    case 2:
+                        gasolina++;
+                        break;
+                    case 3:
+                        diesel++;
+                        break;
+                }
+
+                combustivel = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: {0}", alcool);
+            Console.WriteLine("Gasolina: {0}", gasolina);
+            Console.WriteLine("Diesel: {0}", diesel);
+        }
+    }
+}
