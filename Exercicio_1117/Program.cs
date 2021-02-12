5e8b37c8bc0f458ae3d52ac1ad2fb0f6a533f0da
rafaelvsmello <rmellov@gmail.com>
2021-02-12 19:32:22 -0300

    Commit dos exercício 1114 e 1117

---
+++ Exercicio_1117/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media;
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);                        
+
+            while (nota1 < 0.0 || nota1 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (nota2 < 0.0 || nota2 > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = (nota1 + nota2) / 2;
+
+            Console.WriteLine("media = {0}", media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
