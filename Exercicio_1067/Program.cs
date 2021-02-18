8dd806a569b8b16cecbebc5992beae6799edd065
rafaelvsmello <rmellov@gmail.com>
2021-02-18 17:55:43 -0300

    Commit dos exercício 1067 e 1071

---
+++ Exercicio_1067/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
