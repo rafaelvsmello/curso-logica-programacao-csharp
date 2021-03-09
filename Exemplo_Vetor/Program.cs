2574f3390f45cb913309c2d6ebc545802621beee
rafaelvsmello <rmellov@gmail.com>
2021-03-09 20:34:05 -0300

    Commit exercício vetor

---
+++ Exemplo_Vetor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exemplo_Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet;
+            vet = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
