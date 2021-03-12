6ae3e530dfb74b119af51ee9c7f96f66ee6d4ae4
rafaelvsmello <rmellov@gmail.com>
2021-03-11 21:10:38 -0300

    Commit do exemplo de vetor 5

---
+++ Exemplo_Vetor_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exemplo_Vetor_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] nomes = new string[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                nomes[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("** Nomes lidos **");
+
+            foreach(string elemento in nomes)
+            {
+                Console.WriteLine(elemento);
+            }
+        }
+    }
+}
